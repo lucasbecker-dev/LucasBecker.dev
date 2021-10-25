f1ea44af0380857c1b6b621f12e2a7b28953c982
lucasbecker451 <lucasbecker451@gmail.com>
2021-10-24 20:16:10 -0400

    progress

--- js/color-scheme-controller.js
+++ js/color-scheme-controller.js
@@ -2,12 +2,13 @@
 let darkModeEnabled = false;
 let themeRuleIndex = 0;
 const styleSheet = document.styleSheets[0];
+const styleSheetRules = styleSheet.cssRules;
 const bannerImg = document.getElementById('header-img');
 const osTheme = window.matchMedia("(prefers-color-scheme: dark)");
 const darkBannerImgPath = "../img/dark-theme/default.svg";
 const lightBannerImgPath = "../img/light-theme/default.svg";
 const darkThemeCssRule = ':root {--deep-color: #1e1e1e; --medium-color: #404040; --top-color: #707070; --text-color: #d4d4d4; --green-color: #6a9955; --dark-blue-color: #569cd6; --light-blue-color: #9cdcfe; --red-color: #ce9178; --teal-color: #b5cea8; --yellow-color: #d7ba7d; --purple-color: #c586c0;}';
-const lightThemeCssRule = ':root {--deep-color: #ffffff; --medium-color: #d3d3d3; --top-color: #939393; --text-color: #000000; --green-color: #008000; --dark-blue-color: #0000ff; --light-blue-color: #0070c1; --red-color: #a31515; --teal-color: #098658; --yellow-color: #795e26; --purple-color: #af00db;}'
+const lightThemeCssRule = ':root {--deep-color: #ffffff; --medium-color: #d3d3d3; --top-color: #939393; --text-color: #000000; --green-color: #008000; --dark-blue-color: #0000ff; --light-blue-color: #0070c1; --red-color: #a31515; --teal-color: #098658; --yellow-color: #795e26; --purple-color: #af00db;}';
 
 if (osTheme.matches) {
   darkModeEnabled = true;
@@ -34,6 +35,7 @@ function setDarkTheme() {
   themeRuleIndex = styleSheet.insertRule(darkThemeCssRule);
 }
 
+
 function setLightTheme() {
   darkModeEnabled = false;
   bannerImg.src = lightBannerImgPath;
@@ -41,29 +43,12 @@ function setLightTheme() {
   themeRuleIndex = styleSheet.insertRule(lightThemeCssRule);
 }
 
-// // TODO: remove debug printing lines
-// console.log(darkBannerImg);
-// console.log(lightBannerImg);
-// console.log(header);
-// console.log(header.children);
-// console.log(styleSheet);
-
-// function initDarkBanner() {
-//   const darkBanner = document.createElement('img');
-//   darkBanner.title = bannerImgTitle;
-//   darkBanner.alt = bannerImgAlt;
-//   darkBanner.id = bannerImgId;
-//   darkBanner.class = bannerImgClass;
-//   darkBanner.src = "img/dark-theme/default.svg"
-//   return darkBanner;
-// }
-
-// function initLightBanner() {
-//   const lightBanner = document.createElement('img');
-//   lightBanner.title = bannerImgTitle;
-//   lightBanner.alt = bannerImgAlt;
-//   lightBanner.id = bannerImgId;
-//   lightBanner.class = bannerImgClass;
-//   lightBanner.src = "img/light-theme/default.svg";
-//   return lightBanner;
-// }
+function findCssRules() {
+  let sliderDefaultPositionRuleIndex = -1;
+  for (let i = 0; i < styleSheetRules.length; ++i) {
+    if (styleSheetRules[i].cssText === '.slider:before') {
+      sliderDefaultPositionRuleIndex = i;
+      break;
+    }
+  }
+}
