374085ba0a9ba6a542219a9a4218d42cd524df39
lucasbecker451 <lucasbecker451@gmail.com>
2021-10-31 18:02:46 -0400

    updated formatter to prettier - seems ok

--- js/color-scheme-controller.js
+++ js/color-scheme-controller.js
@@ -5,17 +5,19 @@ let darkModeEnabled = false;
 let themeRuleIndex = 0;
 const styleSheet = document.styleSheets[0];
 const styleSheetRules = styleSheet.cssRules;
-const bannerImg = document.getElementById('header-img');
-const navTextImg = document.getElementById('nav-text');
-const toggleButton = document.getElementById('dark-light-checkbox');
+const bannerImg = document.getElementById("header-img");
+const navTextImg = document.getElementById("nav-text");
+const toggleButton = document.getElementById("dark-light-checkbox");
 const osTheme = window.matchMedia("(prefers-color-scheme: dark)");
 const sliderCssSelectorText = ".slider::before";
 const darkBannerImgPath = "../img/dark-theme/default.svg";
 const lightBannerImgPath = "../img/light-theme/default.svg";
 const darkNavTextImgPath = "../img/dark-theme/isolated-monochrome-white.svg";
 const lightNavTextImgPath = "../img/light-theme/isolated-monochrome-black.svg";
-const darkThemeCssRule = ':root {--deep-color: #1e1e1e; --medium-color: #404040; --top-color: #707070; --text-color: #d4d4d4; --green-color: #6a9955; --dark-blue-color: #569cd6; --light-blue-color: #9cdcfe; --red-color: #ce9178; --teal-color: #b5cea8; --yellow-color: #d7ba7d; --purple-color: #c586c0;}';
-const lightThemeCssRule = ':root {--deep-color: #ffffff; --medium-color: #d3d3d3; --top-color: #939393; --text-color: #000000; --green-color: #008000; --dark-blue-color: #0000ff; --light-blue-color: #0070c1; --red-color: #a31515; --teal-color: #098658; --yellow-color: #795e26; --purple-color: #af00db;}';
+const darkThemeCssRule =
+  ":root {--deep-color: #1e1e1e; --medium-color: #404040; --top-color: #707070; --text-color: #d4d4d4; --green-color: #6a9955; --dark-blue-color: #569cd6; --light-blue-color: #9cdcfe; --red-color: #ce9178; --teal-color: #b5cea8; --yellow-color: #d7ba7d; --purple-color: #c586c0;}";
+const lightThemeCssRule =
+  ":root {--deep-color: #ffffff; --medium-color: #d3d3d3; --top-color: #939393; --text-color: #000000; --green-color: #008000; --dark-blue-color: #0000ff; --light-blue-color: #0070c1; --red-color: #a31515; --teal-color: #098658; --yellow-color: #795e26; --purple-color: #af00db;}";
 
 // site color scheme options
 const themes = {
@@ -27,12 +29,13 @@ const themes = {
 initializeTheme();
 // end main
 
-
 // funcs
 function initializeTheme() {
-  if (osTheme.matches) { // DARK THEME
+  if (osTheme.matches) {
+    // DARK THEME
     clickThemeToggle();
-  } else { // LIGHT THEME
+  } else {
+    // LIGHT THEME
     setTheme(themes.light);
   }
 }
