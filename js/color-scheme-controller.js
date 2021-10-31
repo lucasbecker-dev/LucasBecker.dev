c71c06b57cec075ef73342132f1fda4c4e7e2d85
lucasbecker451 <lucasbecker451@gmail.com>
2021-10-31 17:49:49 -0400

    adjusted nav bar look and switched name to svg

--- js/color-scheme-controller.js
+++ js/color-scheme-controller.js
@@ -6,11 +6,14 @@ let themeRuleIndex = 0;
 const styleSheet = document.styleSheets[0];
 const styleSheetRules = styleSheet.cssRules;
 const bannerImg = document.getElementById('header-img');
+const navTextImg = document.getElementById('nav-text');
 const toggleButton = document.getElementById('dark-light-checkbox');
 const osTheme = window.matchMedia("(prefers-color-scheme: dark)");
 const sliderCssSelectorText = ".slider::before";
 const darkBannerImgPath = "../img/dark-theme/default.svg";
 const lightBannerImgPath = "../img/light-theme/default.svg";
+const darkNavTextImgPath = "../img/dark-theme/isolated-monochrome-white.svg";
+const lightNavTextImgPath = "../img/light-theme/isolated-monochrome-black.svg";
 const darkThemeCssRule = ':root {--deep-color: #1e1e1e; --medium-color: #404040; --top-color: #707070; --text-color: #d4d4d4; --green-color: #6a9955; --dark-blue-color: #569cd6; --light-blue-color: #9cdcfe; --red-color: #ce9178; --teal-color: #b5cea8; --yellow-color: #d7ba7d; --purple-color: #c586c0;}';
 const lightThemeCssRule = ':root {--deep-color: #ffffff; --medium-color: #d3d3d3; --top-color: #939393; --text-color: #000000; --green-color: #008000; --dark-blue-color: #0000ff; --light-blue-color: #0070c1; --red-color: #a31515; --teal-color: #098658; --yellow-color: #795e26; --purple-color: #af00db;}';
 
@@ -51,12 +54,14 @@ function setTheme(theme) {
     case themes.dark:
       darkModeEnabled = true; // must be first
       bannerImg.src = darkBannerImgPath;
+      navTextImg.src = darkNavTextImgPath;
       styleSheet.deleteRule(themeRuleIndex);
       themeRuleIndex = styleSheet.insertRule(darkThemeCssRule);
       break;
     case themes.light:
       darkModeEnabled = false; // must be first
       bannerImg.src = lightBannerImgPath;
+      navTextImg.src = lightNavTextImgPath;
       styleSheet.deleteRule(themeRuleIndex);
       themeRuleIndex = styleSheet.insertRule(lightThemeCssRule);
       break;
