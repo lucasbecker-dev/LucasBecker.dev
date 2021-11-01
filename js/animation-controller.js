362a4c28c08baa49766a48253958fdc5c9a565e0
lucasbecker451 <lucasbecker451@gmail.com>
2021-11-01 15:17:12 -0400

    began laying groundwork for animation controller

--- js/animation-controller.js
+++ js/animation-controller.js
@@ -1,1 +1,6 @@
 // Copyright ©️ Lucas Becker 2021. All right reserved.
+
+// initialization
+const elementsToAnimate =
+  document.getElementsByClassName("slide-and-fade-in-from-left") +
+  document.getElementsByClassName("slide-and-fade-in-from-right");
