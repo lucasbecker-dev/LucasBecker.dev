1dde3a2d118f62de23568632bb1981a3c9d7f699
lucasbecker-dev <lucas@lucasbecker.dev>
2021-11-12 00:26:12 -0500

    getting burger menu to close on clicking a link

--- js/index.js
+++ js/index.js
@@ -6,9 +6,24 @@
 // BURGER MENU FUNCTIONALITY
 const burgerMenu = document.getElementById("burgerMenu");
 const navbarMenu = document.getElementById("navbarMenu");
-
-burgerMenu.addEventListener("click", () => {
+const menuButtons = document.getElementsByClassName("toggles-menu");
+const active = burgerMenu.classList.contains("is-active");
+function toggleActive() {
   burgerMenu.classList.toggle("is-active");
   navbarMenu.classList.toggle("is-active");
+};
+
+burgerMenu.addEventListener("click", () => {
+  if (burgerMenu.classList.contains("is-active")) {
+    for (let button of menuButtons) {
+      button.addEventListener("click", toggleActive());
+    }
+  } else {
+    for (let button of menuButtons) {
+      button.removeEventListener("click", toggleActive());
+    }
+  }
+  this.toggleActive();
 });
+
 // END OF BURGER MENU FUNCTIONALITY
