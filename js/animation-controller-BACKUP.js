36d5db3e2ab8512889cdf2cd916dc65e36af871f
lucasbecker-dev <lucas@lucasbecker.dev>
2021-11-11 20:45:20 -0500

    started adding content for hosting project cards

--- js/animation-controller-BACKUP.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Lucas Becker 2021. All right reserved.
-(function () {
-  "use strict";
-}());
-// initialization
-const elementsToAnimate =
-  document.getElementsByClassName("slide-and-fade-in-from-left") +
-  document.getElementsByClassName("slide-and-fade-in-from-right");
