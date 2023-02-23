c02bb738270b9bd77053614774b5ee2667082d5b
Lucas Becker <lucasbecker451@gmail.com>
2023-02-22 23:03:14 -0500

    re-ran VS Code formatters on all files

--- js/index.js
+++ js/index.js
@@ -1,30 +1,30 @@
 // Copyright (c) Lucas Becker 2021-2022. All right reserved.
 (function () {
-  "use strict";
-}());
+  'use strict';
+})();
 
 // BURGER MENU FUNCTIONALITY
-const burgerMenu = document.getElementById("burgerMenu");
-const navbarMenu = document.getElementById("navbarMenu");
-const menuButtons = document.getElementsByClassName("toggles-menu");
-let active = burgerMenu.classList.contains("is-active");
+const burgerMenu = document.getElementById('burgerMenu');
+const navbarMenu = document.getElementById('navbarMenu');
+const menuButtons = document.getElementsByClassName('toggles-menu');
+let active = burgerMenu.classList.contains('is-active');
 const toggleMenu = () => {
-  active = burgerMenu.classList.toggle("is-active");
-  navbarMenu.classList.toggle("is-active");
+  active = burgerMenu.classList.toggle('is-active');
+  navbarMenu.classList.toggle('is-active');
   toggleButtons();
-}
+};
 const toggleButtons = () => {
   if (active) {
     for (const button of [...menuButtons]) {
-      button.addEventListener("click", toggleMenu);
+      button.addEventListener('click', toggleMenu);
     }
   } else {
     for (const button of [...menuButtons]) {
-      button.removeEventListener("click", toggleMenu);
+      button.removeEventListener('click', toggleMenu);
     }
   }
-}
+};
 
-burgerMenu.addEventListener("click", toggleMenu);
+burgerMenu.addEventListener('click', toggleMenu);
 
 // END OF BURGER MENU FUNCTIONALITY
